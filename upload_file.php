890177e2a794c7920a318993edc8f9bdf314f55d
Andrew Boylan <andreweboylan@gmail.com>
2025-08-19 20:49:14 -0400

    reload php page

--- upload_file.php
+++ upload_file.php
@@ -16,7 +16,7 @@ if ($uploadOk == 0) {
   } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
       //echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-      header("Location: index.html");
+      header("Location: index.php");
       exit();
     } else {
       echo "Sorry, there was an error uploading your file.";
