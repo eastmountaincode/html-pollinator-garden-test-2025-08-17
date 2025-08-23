6f0719ff547f3c9a2302d31ada6c2a6dcc59b96b
Andrew Boylan <andreweboylan@gmail.com>
2025-08-23 13:23:07 -0400

    separate disable box booleans

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
     <div id="box1" class="library-box">
         <marquee>Braids of grain spin wildly; a room with no walls is collecting, pooling in the mirror. A spigot becomes available.</marquee>
         <form action="/upload_file.php" method="post" enctype="musltipart/form-data" class="flex-container" onsubmit="return checkSize(this)">
-            <input id="fileInput1" type="file" name="fileToUpload" required onchange="toggleButton(this)" <?php echo $disabledBoxFull; ?>>
+            <input id="fileInput1" type="file" name="fileToUpload" required onchange="toggleButton(this)" <?php echo $disabledBoxFull1; ?>>
             <input type="hidden" name="box" value="box1">
-            <input id="submitBtn1" style="width: 100px" type="submit" value="Upload" disabled <?php echo $disabledBoxFull; ?>>
+            <input id="submitBtn1" style="width: 100px" type="submit" value="Upload" disabled <?php echo $disabledBoxFull1; ?>>
         </form>
         <?php 
             $dir = 'file-storage/box1'; //define the directory name
@@ -25,10 +25,10 @@
             
             if ($file) {
                 echo "<p>file in box1: $file</p>";
-                $disabledBoxFull = "disabled";  // file already exists
+                $disabledBoxFull1 = "disabled";  // file already exists
             } else {
                 echo "<p>empty</p>";
-                $disabledBoxFull = "";          // allow upload
+                $disabledBoxFull1 = "";          // allow upload
             }
         ?>
 
@@ -36,9 +36,9 @@
     <div id="box2" class="library-box library-box-not-top">
         <marquee>Braids of grain spin wildly; a room with no walls is collecting, pooling in the mirror. A spigot becomes available.</marquee>
         <form action="/upload_file.php" method="post" enctype="musltipart/form-data" class="flex-container" onsubmit="return checkSize(this)">
-            <input type="file" name="fileToUpload" required onchange="toggleButton(this)" <?php echo $disabledBoxFull; ?>>
+            <input type="file" name="fileToUpload" required onchange="toggleButton(this)" <?php echo $disabledBoxFull2; ?>>
             <input type="hidden" name="box" value="box2">
-            <input id="submitBtn" style="width: 100px" type="submit" value="Upload" disabled <?php echo $disabledBoxFull; ?>>
+            <input id="submitBtn" style="width: 100px" type="submit" value="Upload" disabled <?php echo $disabledBoxFull2; ?>>
         </form>
         <?php 
             $dir = 'file-storage/box2';
@@ -50,11 +50,11 @@
             
             if ($file) {
                 echo "<p>file in box2: $file</p>";
-                $disabledBoxFull = "disabled";  // file already exists
+                $disabledBoxFull2 = "disabled";  // file already exists
 
             } else {
                 echo "<p>box2: empty</p>";
-                $disabledBoxFull = "";
+                $disabledBoxFull2 = "";
             }
         ?>
 
