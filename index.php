71d7bdfeb03dbd9f4f5a5a181defa79615602207
Andrew Boylan <andreweboylan@gmail.com>
2025-08-23 16:36:14 -0400

    offer

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
         <form action="/upload_file.php" method="post" enctype="multipart/form-data" class="flex-container" onsubmit="return checkSize(this)">
             <input id="fileInput1" type="file" name="fileToUpload" required onchange="toggleButton(this, 'submitBtn1')" <?php echo $disabledUploadBoxFull1; ?>>
             <input type="hidden" name="box" value="box1">
-            <input id="submitBtn1" type="submit" value="Upload" disabled <?php echo $disabledUploadBoxFull1; ?>>
+            <input id="submitBtn1" type="submit" value="Offer" disabled <?php echo $disabledUploadBoxFull1; ?>>
         </form>
 
         <?php if ($file): ?>
@@ -75,7 +75,7 @@
         <form action="/upload_file.php" method="post" enctype="multipart/form-data" class="flex-container" onsubmit="return checkSize(this)">
             <input type="file" name="fileToUpload" required onchange="toggleButton(this, 'submitBtn2')" <?php echo $disabledUploadBoxFull2; ?>>
             <input type="hidden" name="box" value="box2">
-            <input id="submitBtn" type="submit" value="Upload" disabled <?php echo $disabledUploadBoxFull2; ?>>
+            <input id="submitBtn" type="submit" value="Offer" disabled <?php echo $disabledUploadBoxFull2; ?>>
         </form>
         <?php if ($file): ?>
             <form action="download_file.php" method="get" target="_blank" onsubmit="setTimeout(()=>location.reload(), 2000)">
