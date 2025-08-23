a8cf2f065546537ce96a1af2f0b5103cb3063909
Andrew Boylan <andreweboylan@gmail.com>
2025-08-23 17:02:42 -0400

    use __DIR__ to get the project folder root

--- upload_file.php
+++ upload_file.php
@@ -1,6 +1,6 @@
 <?php
 $target_box = $_POST["box"];
-$target_dir = "/file-storage/$target_box/";
+$target_dir = __DIR__ . "/file-storage/$target_box/";  // correct path inside project
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $max_size_mb = 25 * 1024 * 1024; // 1 kilobtypes (KB) = 1024 bytes, and 1 megabyte (MB) = 1024 kilobytes (KB)
 
